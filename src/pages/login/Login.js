579494f82a420a14b96e6baf6c22c60e1b554be3
v-zhehaoan@microsoft.com <v-zhehaoan@microsoft.com>
2022-10-01 17:00:17 -0700

    dev

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -57,7 +57,7 @@ function Login(props) {
           {activeTabId === 0 && (
             <React.Fragment>
               <Typography variant="h1" className={classes.greeting}>
-                Good Morning, User, here is dev site, merging testsetsetset.
+                Good Morning, User, here is dev site.
               </Typography>
               <Button size="large" className={classes.googleButton}>
                 <img src={google} alt="google" className={classes.googleIcon} />
