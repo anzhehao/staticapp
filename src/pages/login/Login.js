769a26f9551cb3b0994d54fb0999139a95b647a2
v-zhehaoan@microsoft.com <v-zhehaoan@microsoft.com>
2022-10-01 17:24:38 -0700

    final

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -57,7 +57,7 @@ function Login(props) {
           {activeTabId === 0 && (
             <React.Fragment>
               <Typography variant="h1" className={classes.greeting}>
-                Good Morning, User, here is dev site.
+                Good Morning, User, here is dev site, final check.
               </Typography>
               <Button size="large" className={classes.googleButton}>
                 <img src={google} alt="google" className={classes.googleIcon} />
